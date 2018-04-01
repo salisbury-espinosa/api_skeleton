1f72cdab5ba8d32a5a962e2adcc487a127a491e6
salisbury-espinosa <salisbury.espinosa@gmail.com>
2018-04-02 02:02:49 +0300

    added helmet package for securiy issues

--- server/middleware/Routes.js
+++ server/middleware/Routes.js
@@ -1,6 +1,7 @@
 // @flow
 import bodyParser from "body-parser";
 import compress from "compression";
+import helmet from "helmet";
 import express, {
   type $Request,
   type $Response,
@@ -25,6 +26,18 @@ export default class Routes {
   _setupExpress(controllers?: Array<Object>): void {
     const { app } = Api;
     app.use(
+      helmet({
+        frameguard: {
+          action: "deny"
+        },
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: ["'none'"]
+          }
+        }
+      })
+    );
+    app.use(
       bodyParser.urlencoded({
         extended: true
       })
@@ -38,9 +51,6 @@ export default class Routes {
       })
     );
     app.use(responseTime());
-
-    app.use(this._addSecHeaders);
-    app.use(this._removeSecHeaders);
 
     if (controllers) {
       this._registerControllers(controllers);
@@ -87,36 +97,5 @@ export default class Routes {
     }
 
     return res.status(500).json(body);
-  }
-
-  /**
-   * Add security sensitive headers.
-   * @see https://github.com/shieldfy/API-Security-Checklist#output
-   * @param {!Object} req - The ExpressJS request object.
-   * @param {!Object} res - The ExpressJS response object.
-   * @param {!Function} next - The ExpressJS next function.
-   * @returns {undefined}
-   */
-  _addSecHeaders(req: $Request, res: $Response, next: NextFunction): mixed {
-    res.setHeader("X-Content-Type-Options", "no-sniff");
-    res.setHeader("X-Frame-Options", "deny");
-    res.setHeader("Content-Security-Policy", "default-src: 'none'");
-
-    return next();
-  }
-
-  /**
-   * Remove security sensitive headers.
-   * @see https://github.com/shieldfy/API-Security-Checklist#output
-   * @param {!Object} req - The ExpressJS request object.
-   * @param {!Object} res - The ExpressJS response object.
-   * @param {!Function} next - The ExpressJS next function.
-   * @returns {undefined}
-   */
-  _removeSecHeaders(req: $Request, res: $Response, next: NextFunction): mixed {
-    res.removeHeader("X-Powered-By");
-    res.removeHeader("Server");
-
-    return next();
   }
 }

8cc0e4a997d6566b873b78dfc34e980ed1524a6e
salisbury-espinosa <salisbury.espinosa@gmail.com>
2018-04-02 02:03:15 +0300

    changed @flow type for asyncUtilWrap

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -2,10 +2,8 @@
 import type { $Response, $Request, NextFunction } from "express";
 
 // eslint-disable-next-line import/prefer-default-export
-export const asyncUtilWrap = (fn: Function) => (
-  req: $Request,
-  res: $Response,
-  next: NextFunction
-): mixed => {
+export const asyncUtilWrap = (
+  fn: (req: $Request, res: $Response, next: NextFunction) => mixed
+) => (req: $Request, res: $Response, next: NextFunction): mixed => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
